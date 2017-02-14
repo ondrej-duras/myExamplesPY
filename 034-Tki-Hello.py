d509ccb0956c55a8868f5ce33fbdbb3749998680
Ondrej DURAS <ondrej.duras@hpe.com>
2017-02-14 11:37:58 +0100

    ako na Tk v Pythone - je sucast Py instalacky

---
+++ 034-Tki-Hello.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+# http://www.programcreek.com/python/index/module/list # zoznam najuspesnejsich balickov Py
+# http://www.python-course.eu/python_tkinter.php       # kurz ku Tk (sucast instalacky  Py)
+# http://www.python-course.eu/tkinter_dialogs.php      # specialne ku dialogom
+
+
+from Tkinter import *
+from tkMessageBox import *
+
+def answer():
+    showerror("Answer", "Sorry, no answer available")
+
+def callback():
+    if askyesno('Verify', 'Really quit?'):
+        showwarning('Yes', 'Not yet implemented')
+    else:
+        showinfo('No', 'Quit has been cancelled')
+
+Button(text='Quit', command=callback).pack(fill=X)
+Button(text='Answer', command=answer).pack(fill=X)
+mainloop()
+
+# --- koniec ---
+
---
+++ 035-Tki-Canvas.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+# http://www.python-course.eu/python_tkinter.php  # manual ku Tk balicku, ktory je sucastou installacky Py
+# http://www.python-course.eu/tkinter_canvas.php  # special ku Canvas/Platnu na kreslenie
+
+from Tkinter import *
+master = Tk()
+
+WIDTH  = 640
+HEIGHT = 480
+w = Canvas(master, 
+           width  = WIDTH  +1,
+           height = HEIGHT +1)
+w.pack()
+
+w.create_line(    0,      0, WIDTH,      0, fill="#ff2020")
+w.create_line(WIDTH,      0, WIDTH, HEIGHT, fill="#20ff20")
+w.create_line(WIDTH, HEIGHT,     0, HEIGHT, fill="#2020ff")
+w.create_line(    0, HEIGHT,     0,      0, fill="#ffff20")
+
+w.create_line(    0,      0, WIDTH, HEIGHT, fill="#20ffff")
+w.create_line(WIDTH,      0,     0, HEIGHT, fill="#ff20ff")
+
+
+mainloop()
+
