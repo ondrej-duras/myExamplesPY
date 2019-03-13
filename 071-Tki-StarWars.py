56ba81d02ef510b628bbf256d18a7422567b71b9
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2019-03-13 20:15:17 +0100

    since now it's possible to shut satelites

--- 071-Tki-StarWars.py
+++ 071-Tki-StarWars.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 
+VERSION = 2019.031301
+
 import Tkinter as tk
 import time
 import random
@@ -8,6 +10,8 @@ random.seed(time.time())
 QUIT = False
 myxx = 350
 myyy = 450
+mydx = 3 
+mydy = 3
 VECI = []
 VECT = 0
 
@@ -92,9 +96,10 @@ SAT4 = """
 {#088 #088 #088 #088 #088 #000 #000 #000 #000}
 """
 
-SATANIM  = [SAT1,SAT2,SAT3,SAT4]
-SATELITS = []
-SATLIMIT = 10
+SATANIM  = [SAT1,SAT2,SAT3,SAT4]   # zoznam animacnych obrazkov
+SATELITS = []   # zoznam satelitov
+SATLIMIT = 10   # maximalny pocet satelitov na obrazovke
+FIRES = []     # pole so strelami
 
 def satUpdate():
   global SATELITS,SATLIMIT,img
@@ -103,7 +108,8 @@ def satUpdate():
            'dx':random.uniform(-5.0,5.0), 
            'dy':random.uniform(0.0,10.0),
            'delete':False,
-           'anim' : random.randint(0,3)
+           'anim' : random.randint(0,3),
+           'animct' : random.randint(1,10)
           }
     SATELITS.append(sat)
   for i in range(len(SATELITS)):
@@ -121,12 +127,32 @@ def satUpdate():
     i += 1
   for i in range(len(SATELITS)):
     sat = SATELITS[i]
-    img.put(SATANIM[sat['anim']],(int(sat['x']),int(sat['y'])))
+    animct = sat['animct']
+    anim   = sat['anim']
+    img.put(SATANIM[anim],(int(sat['x']),int(sat['y'])))
+    animct -= 1
+    if animct < 1 :
+       animct = random.randint(1,10)
+       x = random.randint(0,3)
+       if x == 1 : anim = (anim+1) % 4 # finta +1
+       if x == 2 : anim = (anim+3) % 4 # finta -1
+       SATELITS[i]['anim'] = anim
+    SATELITS[i]['animct'] = animct
 
 
-FIRES = []
 
 def fireCheck(x,y):
+  sats = len(SATELITS) - 1; i = 0
+  while i < sats:
+     i += 1
+     sx,sy = SATELITS[i]['x'],SATELITS[i]['y']
+     if x < sx    : continue
+     if x > sx+14 : continue
+     if y < sy    : continue
+     if y > sy+20 : continue
+     #SATELITS[i]['delete'] = True
+     del SATELITS[i]
+     return 1
   return 0
 
 def fireAdd():
@@ -164,24 +190,31 @@ def fireUpdate():
     ix += 1
 
  
-
-
-
 def koniec():
   global QUIT
   QUIT=True
 
 def klavesy(event):
   global myxx,myyy
+  global mydx,mydy
   key = event.char
   kod = event.keycode
   if kod == 27 : koniec()
-  if kod == 37 : myxx -= 3
-  if kod == 39 : myxx += 3 
-  if kod == 38 : myyy -= 3
-  if kod == 40 : myyy += 3
+  if kod == 37 : myxx -= mydx ; mydx += 1
+  if kod == 39 : myxx += mydx ; mydx += 1
+  if kod == 38 : myyy -= mydy ; mydy += 1
+  if kod == 40 : myyy += mydy ; mydy += 1
   if kod == 32 : fireAdd()
   #print kod
+
+def pustenie(event):
+  global mydx,mydy
+  kod = event.keycode
+  if kod == 37 : mydx = 3
+  if kod == 39 : mydx = 3
+  if kod == 38 : mydy = 3
+  if kod == 40 : mydy = 3
+
 
 wnd = tk.Tk()
 wnd.title("Star Wars IV - Imperium utoci")
@@ -194,6 +227,7 @@ can.create_image((320,225),image=img,state="normal",anchor=tk.CENTER)
 can.place(x=0,y=0)
 wnd.protocol('WM_DELETE_WINDOW',koniec)
 wnd.bind('<KeyPress>',klavesy)
+wnd.bind('<KeyRelease>',pustenie)
 
 while not QUIT:
   img.put("#000",(30,30,670,482))
