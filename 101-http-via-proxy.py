f04069b2a7d2c7c006af303a4f659e8dee167903
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2020-06-12 14:44:26 +0200

    from corporation to INET via proxy
    from corporation into the open world via proxy - the most simple way (asks for one password)

---
+++ 101-http-via-proxy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+"""
+Description:
+ Example how to access outside world from
+ corporation via proxy.
+ An issue is a 'requests' package as it is
+ not python2 native.
+"""
+
+
+import requests
+import getpass
+
+from requests.auth import HTTPProxyAuth
+
+user = getpass.getuser()
+pasw = getpass.getpass("Password(%s):" % (user))
+proxies = {"http":"10.2.99.77:80"}
+auth = HTTPProxyAuth(user,pasw)
+
+http = requests.get("http://www.google.com/", proxies=proxies, auth=auth)
+print http.text
+
+
+# --- end ---
+
