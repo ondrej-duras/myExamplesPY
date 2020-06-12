27cadb00ef4a495dbeb61f2f98d2d52adfc0739a
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2020-06-12 15:02:13 +0200

    simple way from corporation to INET via proxy
    unfortunately the requests module is not native.
    You have to download it first.
    But some alternative via native urllib should exist

--- 101-http-via-proxy.py
+++ 101-http-via-proxy.py
@@ -16,7 +16,7 @@ from requests.auth import HTTPProxyAuth
 
 user = getpass.getuser()
 pasw = getpass.getpass("Password(%s):" % (user))
-proxies = {"http":"10.2.99.77:80"}
+proxies = {"http":"10.0.0.1:80"}
 auth = HTTPProxyAuth(user,pasw)
 
 http = requests.get("http://www.google.com/", proxies=proxies, auth=auth)
