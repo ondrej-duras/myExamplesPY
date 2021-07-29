8a65207a881c76230a31b0dfcb24cde8d7335ca0
Ing. Ondrej ĎURÁS (dury) <ondrej-duras@users.noreply.github.com>
2021-07-29 21:40:20 +0200

    Simple Telnet Session against switch like Cat3750

---
+++ 118-Net-Telnet-Session-Hello.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# 20210729, Ondrej DURAS (dury)
+# Very basic Telnet session
+# 1st - uses raw strings
+# 2nd - uses sets of regular expressions
+# to identify the prompt (end of command output)
+#
+
+import getpass
+import sys
+import os
+import telnetlib
+
+
+HOST="L-001-BB-AC-0101"
+try:
+  USER=os.environ["SSHUSER"]
+except:
+  pass
+
+if not USER:
+  if sys.version_info.major == 2:
+    USER = raw_input("Login: ")
+  else:
+    USER = input("Login: ")
+
+PASS=getpass.getpass("Password[%s]: " % USER)
+
+
+tel = telnetlib.Telnet(HOST,23,10) # 23/tcp,10sec.
+tel.read_until("Username: ")
+tel.write(USER + "\r\n")
+tel.read_until("Password: ")
+tel.write(PASS + "\r\n")
+
+tel.write("terminal length 0\r\n")
+print(tel.read_until(HOST+"#",10)) # sent command in echo
+
+tel.write("show int status\r\n")
+#print tel.read_all()
+#print tel.expect(["^"+HOST+"#"],10)[2].replace(r"\r\n","\n")
+print(tel.read_until(HOST+"#",10))
+print(tel.read_until(HOST+"#",10))
+
+tel.write("show mac address-table\r\n") # command
+#print tel.expect(["^"+HOST+"#"],10)[2].replace(r"\r\n","\n")
+print(tel.read_until(HOST+"#",10))
+#print tel.read_until(HOST+"#",10)
+
+#print tel.read_until(HOST+"#",10)
+tel.write("exit\r\n")
+tel.close()
+
+# ------
+print "#"*70
+
+# Opening and Authenticating a session
+tel = telnetlib.Telnet(HOST,23,10)
+tel.expect(["[Ll]ogin: ?","[Uu]ser(name)?: ?"],15)
+tel.write(USER + "\r\n")
+tel.expect(["[Pp]assword: ?"],15)
+tel.write(PASS + "\r\n")
+tel.write("terminal length 0\r\n")
+tel.expect([""+HOST+"#"],10)
+
+tel.write("show int status\r\n")
+print(tel.expect([""+HOST+"#"],10)[2].replace(r"\r\n","\n"))
+print(tel.expect([""+HOST+"#"],10)[2].replace(r"\r\n","\n"))
+
+tel.write("show mac address-table\r\n")
+print(tel.expect([""+HOST+"#"],10)[2].replace(r"\r\n","\n"))
+#print tel.expect([""+HOST+"#"],10)[2].replace(r"\r\n","\n")
+
+
+tel.write("exit\r\n")
+tel.close()
+
+# --- end ---
+
